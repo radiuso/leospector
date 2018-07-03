ead4e1129920da826da5cda173ccbe8d1e75bb8b
ITGCE\S0079569 <thomas.vandermeersch.prestataire@it-ce.fr>
2018-07-03 17:07:20 +0200

    eject + add bulma

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="container">
+        <div className="hero is-primary">
+        <div className="hero-body has-text-centered">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1 className="title">Welcome to React</h1>
+          <p className="subtitle">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        </div>
       </div>
     );
   }
